4169a90ead20839b2e80ab606d121a19594f95fa
malikovshahriyor <malikovshahriyor929@gmail.com>
2024-12-09 22:54:44 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        brandgreen: "#a0a747",
+        brandbrown: "#773844",
+        brandblue: "#1f2057",
+        brandblue2: "#465587",
+        brandcyan: "#77dfe9",
+        brandbeige: "#d6d1b2",
+        brandyellow: "#d5b753",
+        brandorange: "#be6833",
+        textprimary: "#1a1a1a",
+        textgrey: "#646562",
+        lineprimary: "#eae3d9",
+        linedark: "#c2bdb6",
+      },
+    },
+  },
+  plugins: [],
+};
