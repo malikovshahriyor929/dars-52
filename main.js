42ba326adbcee5f6981a7e3090a049bf879aad1a
malikovshahriyor <malikovshahriyor929@gmail.com>
2024-12-10 11:12:39 +0500

    sdfc

---
+++ main.js
@@ -0,0 +1,37 @@
+// 1 masala
+// function arr2(a, b) {
+//   num1 = a.join(" ");
+//   num2 = b.join(" ");
+//  number1= 0
+//  number2= 0
+//   for (let i = 0; i < num1.length; i++) {
+//      number1 += num1[i]**2
+//   }
+//   for (let i = 0; i < num2.length; i++) {
+//     number2 += num2[i]**2
+//  }
+//   console.log( number1 > number2 ? true:false)
+// }
+// arr2([4, 5, 6], [1, 2, 3]);
+
+// 2 masala
+// let arr = ["h","o","l","9"]
+// console.log(arr.join());
+
+// 3 masala
+// let num = [1,3,9,7]
+// const res = num.reduce((acc, value)=>(acc+value),0)
+// console.log(res/num.length)
+
+// 4 masala
+// let str = "I love arrays they are my favorite"
+// console.log(str.split(" "))
+
+// 5 masala
+// let students = [4, 5, 3, 2];
+// const score = students.reduce((acc, value) => acc + value);
+// console.log(Math.floor(score / students.length));
+
+// 6 masala
+// let arr = [34, 15, 88, 2]
+// console.log(arr.sort((a,b)=> a -b)[0])
